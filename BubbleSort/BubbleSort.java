192997c53ee068e20ef65355963f257acf82de1c
Mark <markrdecello@gmail.com>
2020-02-01 00:55:20 -0500

    Bubble sort in java

--- BubbleSort/BubbleSort.java
+++ BubbleSort/BubbleSort.java
@@ -1,5 +1,18 @@
 public class BubbleSort{
 
 	public static void main(String[] args){
-		
+		int[] arr = {6, 3, 19, 25, 8, 5};
+		for(int i = 0; i < arr.length-1; i++){
+			for(int j = 0; j < arr.length-i-1; j++){
+				if(arr[j] > arr[j+1]){
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}	
+		}	
+		for(int x = 0; x < arr.length; x++){
+			System.out.print(arr[x] + " ");
+		}
+	}
 }
