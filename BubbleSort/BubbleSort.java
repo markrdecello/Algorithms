dca030679f0c01a77959f5b67b9f8ca8c53e755d
Mark Decello <markyz1044@gmail.com>
2020-02-01 23:36:58 -0500

    Bubble sort in java

--- BubbleSort/BubbleSort.java
+++ BubbleSort/BubbleSort.java
@@ -1,9 +1,16 @@
 public class BubbleSort{
 
 	public static void main(String[] args){
-		int[] arr = {6, 3, 19, 25, 8, 5};
-		for(int i = 0; i < arr.length-1; i++){
-			for(int j = 0; j < arr.length-i-1; j++){
+		int[] arr = {6, 3, 19, 25, -11, 8, 5, 92, 1, 56, -12, 34};
+		System.out.println("Current Numbers:");
+		int arrLen = arr.length-1;
+		System.out.print("\t");
+		for(int x = 0; x <= arrLen; x++){
+			System.out.print(arr[x] + " ");
+		}
+		System.out.println();
+		for(int i = 0; i < arrLen; i++){
+			for(int j = 0; j < arrLen-i; j++){
 				if(arr[j] > arr[j+1]){
 					int temp = arr[j];
 					arr[j] = arr[j+1];
@@ -11,8 +18,11 @@ public class BubbleSort{
 				}
 			}	
 		}	
-		for(int x = 0; x < arr.length; x++){
-			System.out.print(arr[x] + " ");
+		System.out.println("Sorted  Numbers:");
+		System.out.print("\t");
+		for(int y = 0; y <= arrLen; y++){
+			System.out.print(arr[y] + " ");
 		}
+		System.out.println();
 	}
 }
