374205b93de6559330abe3dae8849451c1c19ab3
Mark Decello <markyz1044@gmail.com>
2020-02-05 14:00:14 -0500

    Polish Peasant Multiplication

---
+++ PolishPeasant/PolishPeasant.java
@@ -0,0 +1,32 @@
+public class PolishPeasant{
+
+
+	public static void main(String[] args){
+		int num1 = 999;
+		int num2 = 15;
+		int product = 0;
+
+		//Just to label num1 and num2
+		System.out.println("Num1\tNum2");
+		System.out.println(num1 + "\t" + num2);
+		
+		//Reads if num2 is odd to check whether to add num1 to the total
+		if(num2 % 2 == 1){
+			product += num1;
+		}
+
+		//A loop to continue to read num2 and divide by half until num2 is 1
+		//The if statement continues to add all of num1 when num2 is odd
+		while(num2 != 1){
+			num2 = num2 / 2;
+			num1 = num1 * 2;
+			if(num2 % 2 == 1){
+				product += num1;
+			}
+			System.out.println(num1 + "\t" + num2);
+		}
+		//Just to display the product of the two numbers
+		System.out.println("---------------");
+		System.out.println(product + "\t" + num2);
+	}
+}
